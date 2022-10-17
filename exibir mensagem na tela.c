5ef5f8dcc7f4c15c7ee7f5f4630231473a574593
pronatecbueno <116038897+pronatecbueno@users.noreply.github.com>
2022-10-17 18:58:39 -0300

    Arquivo de C que exibe mensagem na tela

---
+++ exibir mensagem na tela.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    //Comando printf exibe informação
+    printf("Olá Mundo! \n"); //\n faz quebra de linha
+    printf("Segunda mensagem exibida");
+    return 0;
+}
