98112a7fb6015736167fb077d9a81f9bc88bfbbf
pronatecbueno <116038897+pronatecbueno@users.noreply.github.com>
2022-10-18 19:09:57 -0300

    Coloquei comentários no código

--- estrutura_condicional_if_e_else.c
+++ estrutura_condicional_if_e_else.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include <stdio.h>//biblioteca 
 
 int main()
 {
-    float nota;
-    printf("Digite a nota do aluno: ");
-    scanf("%f", &nota);
     
-    if(nota>=6 && nota<=10){
+    float nota;//Declaração de variáveis
+    printf("Digite a nota do aluno: ");//Exibe mensagem na tela
+    scanf("%f", &nota);//salva o número digitado na variável
+    
+    if(nota>=6 && nota<=10){//compara se o aluno foi aprovado
         printf("Aluno Aprovado!");
     }
-    else if(nota>=0 && nota<6){
+    else if(nota>=0 && nota<6){//compara se o aluno foi reprovado
         printf("Aluno reprovado!");
     }
-    else{
+    else{//Verifica se a nota digitada está errada
         printf("A nota digitada está errada!");
     }
 
     return 0;
-}
+}//fim do algoritmo
