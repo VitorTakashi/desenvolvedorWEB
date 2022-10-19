412df6ec7f1c21678d06cb2031c1e371b2ae4ee1
pronatecbueno <116038897+pronatecbueno@users.noreply.github.com>
2022-10-19 19:08:22 -0300

    criar pasta

--- exibir mensagem na tela.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    //Comando printf exibe informação
-    printf("Olá Mundo! \n"); //\n faz quebra de linha
-    printf("Segunda mensagem exibida");
-    return 0;
-}
